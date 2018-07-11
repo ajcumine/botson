cdf807602ce4e48def1d8e5009e38c7ba1f31ef7
ajcumine <ajcumine@gmail.com>
2018-07-11 17:11:58 +0100

    add winston logger

---
+++ src/services/logger/index.ts
@@ -0,0 +1,11 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+    }),
+  ],
+});
+
+export default logger;
